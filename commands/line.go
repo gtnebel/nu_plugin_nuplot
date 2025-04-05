bf679ce9024caff726c5c9148527054c5d56f363
Thomas Nebel <tnebel@vivaldi.net>
2025-04-05 23:31:11 +0200

    work on `nuplot line` function

--- commands/line.go
+++ commands/line.go
@@ -4,6 +4,11 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+	charttypes "github.com/go-echarts/go-echarts/v2/types"
 
 	"github.com/ainvaltin/nu-plugin"
 	"github.com/ainvaltin/nu-plugin/types"
@@ -12,18 +17,21 @@ import (
 func NuplotLine() *nu.Command {
 	return &nu.Command{
 		Signature: nu.PluginSignature{
-			Name:                 "nuplot line",
-			Category:             "Formats",
-			Desc:                 `Plots the data that is piped into the command as 'echarts' graph.`,
-			SearchTerms:          []string{"plot", "graph", "line", "bar"},
-			InputOutputTypes:     []nu.InOutTypes{{In: types.Binary(), Out: types.Any()}, {In: types.String(), Out: types.Any()}},
+			Name:        "nuplot line",
+			Category:    "chart",
+			Desc:        `Plots the data that is piped into the command as 'echarts' graph.`,
+			SearchTerms: []string{"plot", "graph", "line", "bar"},
+			InputOutputTypes: []nu.InOutTypes{
+				{In: types.List(types.Table(types.RecordDef{})), Out: types.Nothing()},
+				{In: types.List(types.Number()), Out: types.Nothing()},
+			},
 			AllowMissingExamples: true,
 		},
 		Examples: nu.Examples{
 			{
-				Description: `Convert an Open Step array to list of Nu values`,
-				Example:     `'(10,foo,)' | from plist`,
-				Result:      &nu.Value{Value: []nu.Value{{Value: 10}, {Value: "foo"}}},
+				Description: `Plot a line graph of an array of numbers.`,
+				Example:     `[5, 4, 3, 2, 5, 7, 8] | nuplot line`,
+				// Result:      &nu.Value{Value: []nu.Value{{Value: 10}, {Value: "foo"}}},
 			},
 		},
 		OnRun: nuplotLineHandler,
@@ -35,24 +43,59 @@ func nuplotLineHandler(ctx context.Context, call *nu.ExecCommand) error {
 	case nil:
 		return nil
 	case nu.Value:
-		// var buf []byte
+		items := make([]opts.LineData, 0)
+
 		switch data := in.Value.(type) {
-		// case []byte:
-		// 	buf = data
-		// case string:
-		// 	buf = []byte(data)
+		case []int64:
+			for _, val := range data {
+				items = append(items, opts.LineData{Value: val})
+			}
+		case []float64:
+			for _, val := range data {
+				items = append(items, opts.LineData{Value: val})
+			}
 		default:
 			return fmt.Errorf("unsupported input value type %T", data)
 		}
-		var v any
+
+		// create a new line instance
+		line := charts.NewLine()
+		// set some global options like Title/Legend/ToolTip or anything else
+		line.SetGlobalOptions(
+			charts.WithInitializationOpts(opts.Initialization{Theme: charttypes.ThemeWesteros}),
+			charts.WithTitleOpts(opts.Title{
+				Title:    "Line example in Westeros theme",
+				Subtitle: "Line chart rendered by the http server this time",
+			}))
+
+		xRange := make([]int, len(items))
+		for i := range len(items) {
+			xRange[i] = i
+		}
+
+		// Put data into instance
+		line.SetXAxis(xRange).
+			AddSeries("Items", items).
+			SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: opts.Bool(true)}))
+
+		f1, _ := os.Create("line.html")
+		line.Render(f1)
+
+		// If the call returns nothing, we can return nil here.
+		return nil
+
+		// var v any
 		// if _, err := plist.Unmarshal(buf, &v); err != nil {
 		// 	return fmt.Errorf("decoding input as plist: %w", err)
 		// }
-		rv, err := asValue(v)
-		if err != nil {
-			return fmt.Errorf("converting to Value: %w", err)
-		}
-		return call.ReturnValue(ctx, rv)
+		// rv, err := asValue(v)
+		// if err != nil {
+		// 	return fmt.Errorf("converting to Value: %w", err)
+		// }
+		// return call.ReturnValue(ctx, rv)
+	case <-chan nu.Value:
+		return fmt.Errorf("unsupported input type %T", call.Input)
+
 	case io.Reader:
 		// decoder wants io.ReadSeeker so we need to read to buf.
 		// could read just enough that the decoder can detect the
@@ -61,15 +104,16 @@ func nuplotLineHandler(ctx context.Context, call *nu.ExecCommand) error {
 		// if err != nil {
 		// 	return fmt.Errorf("reding input: %w", err)
 		// }
-		var v any
+		// var v any
 		// if _, err := plist.Unmarshal(buf, &v); err != nil {
 		// 	return fmt.Errorf("decoding input as plist: %w", err)
 		// }
-		rv, err := asValue(v)
-		if err != nil {
-			return fmt.Errorf("converting to Value: %w", err)
-		}
-		return call.ReturnValue(ctx, rv)
+		// rv, err := asValue(v)
+		// if err != nil {
+		// 	return fmt.Errorf("converting to Value: %w", err)
+		// }
+		// return call.ReturnValue(ctx, rv)
+		return fmt.Errorf("unsupported input type %T", call.Input)
 	default:
 		return fmt.Errorf("unsupported input type %T", call.Input)
 	}
